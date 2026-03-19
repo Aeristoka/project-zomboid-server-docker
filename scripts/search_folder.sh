fb2075236f35f40d160d7ce69787da8e7ad997ce
Aeristoka <jmgriffes@gmail.com>
2026-03-19 17:55:48 -0600

    Update search_folder.sh to match B42 Mod folder structure Allows proper finding of Maps via search_folder.sh, which worked in B41 and does not in B41 (another layer of nesting). Would be good to have some sort of check for server version and search based on that, but I don't yet know a way to complete that.

--- scripts/search_folder.sh
+++ scripts/search_folder.sh
@@ -10,36 +10,38 @@ search_folder() {
         echo "Searching for maps: ($counter/$(ls -1 "$search_dir" | wc -l))"
 
         # Check if the given directory exists
-        if [ -d "$search_dir" ]; then                
+        if [ -d "$search_dir" ]; then
             # Check if there is a "maps" folder within the "mods" directory
             if [ -d "$item/mods" ]; then
                 for mod_folder in "$item/mods"/*; do
-                    if [ -d "$mod_folder/media/maps" ]; then
-                
-                        # Copy maps to map folder
-                        source_dirs=("$mod_folder/media/maps"/*)
-                        map_dir=("${HOMEDIR}/pz-dedicated/media/maps")
+                    for internal_folder in "$mod_folder"/*; do
+                        if [ -d "$internal_folder/media/maps" ]; then
 
-                        for source_dir in "${source_dirs[@]}"; do
-                            dir_name=$(basename "$source_dir")
-                            if [ ! -d "$map_dir/$dir_name" ]; then
-                                echo "Found map(s). Copying..."
-                                cp -r "$mod_folder/media/maps"/* "${HOMEDIR}/pz-dedicated/media/maps"
-                                echo "Successfully copied!"
-                            fi
-                        done
+                            # Copy maps to map folder
+                            source_dirs=("$internal_folder/media/maps"/*)
+                            map_dir=("${HOMEDIR}/pz-dedicated/media/maps")
 
-                        # Adds map names to a semicolon separated list and outputs it.
-                        map_list=""
-                        for dir in "$mod_folder/media/maps"/*/; do
-                            if [ -d "$dir" ]; then
-                                dir_name=$(basename "$dir")
-                                map_list+="$dir_name;"     
-                            fi
-                        done
-                        # Exports to .txt file to add to .ini file in entry.sh
-                            echo -n "$map_list" >> "${HOMEDIR}/maps.txt"
-                    fi
+                            for source_dir in "${source_dirs[@]}"; do
+                                dir_name=$(basename "$source_dir")
+                                if [ ! -d "$map_dir/$dir_name" ]; then
+                                    echo "Found map(s). Copying..."
+                                    cp -r "$internal_folder/media/maps"/* "${HOMEDIR}/pz-dedicated/media/maps"
+                                    echo "Successfully copied!"
+                                fi
+                            done
+
+                            # Adds map names to a semicolon separated list and outputs it.
+                            map_list=""
+                            for dir in "$internal_folder/media/maps"/*/; do
+                                if [ -d "$dir" ]; then
+                                    dir_name=$(basename "$dir")
+                                    map_list+="$dir_name;"     
+                                fi
+                            done
+                            # Exports to .txt file to add to .ini file in entry.sh
+                                echo -n "$map_list" >> "${HOMEDIR}/maps.txt"
+                        fi
+                    done
                 done
             fi
         fi
